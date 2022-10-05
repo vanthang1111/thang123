1de5863b77144fcdbcedb649eb7cc0ec74d6cc8b
vanthang1111 <thangpro9669@gmail.com>
2022-10-05 21:15:16 +0700

    thangdzaiiiiii

---
+++ connect.php
@@ -0,0 +1,1 @@
+<?php $conn = mysqli_connect("localhost", "root", "", "data"); mysqli_set_charset($conn, 'UTF8'); ?>
---
+++ footer.php
@@ -0,0 +1,19 @@
+<div class="jumbotron">
+     
+<div class="appeal">            
+        </div>
+ 
+
+<h2>Copyright @ 2020 Design by Quách Quỳnh</h2>
+ 
+</div>
+ 
+</div>
+ 
+<!--/. Container-->
+</div>
+ 
+</div>
+ 
+</body>
+</html>
